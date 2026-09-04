a352db0d02362703e6e7f40f6513f1974a897734
zamirkrasniqi <zkrasniqii8@gmail.com>
2026-09-04 16:42:44 +0200

    Week 36, level 2

--- Secondproject/Program.cs
+++ Secondproject/Program.cs
@@ -1,3 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter product names.");
+Console.WriteLine("Type 'exit' to finish.");
+Console.WriteLine();
 
-//new code
+List<string> products = new List<string>();
+string input = string.Empty;
+
+while (input.ToLower().Trim() != "exit")
+{
+    Console.Write("Product: ");
+    input = Console.ReadLine();
+
+    if (input.ToLower().Trim() != "exit")
+    {
+        products.Add(input);
+    }
+}
+
+products.Sort();
+Console.WriteLine();
+Console.WriteLine("Sorted product list: ");
+Console.WriteLine();
+
+for (int i = 0; i < products.Count; i++)
+{
+    Console.WriteLine("- " + products[i]);
+}
+
